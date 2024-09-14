28c24926791cd9b8ef373981065a2ad0def2b8c9
Maria <maria.o.fitas@gmail.com>
2024-09-14 20:24:07 +0900

    Previously named UI.groovy
    Changed name, added imports, created main class TextReplacerUI

---
+++ TextReplacerUI.groovy
@@ -0,0 +1,15 @@
+@Grab('org.openjfx:javafx-controls:11')
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.scene.layout.VBox
+import javafx.stage.Stage
+import javafx.scene.paint.Color
+import javafx.scene.layout.HBox
+import javafx.geometry.Insets
+
+class TextReplacerUI extends Application {
+    
+}
